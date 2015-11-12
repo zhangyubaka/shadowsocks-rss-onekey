bd56508e1751f8d8a8141d4931891fd32ac62cc9
mikelei8291 <mikelei8291@gmail.com>
2015-11-13 00:02:12 +0800

    Bug fix

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@ echo "One key script by AmedaGintoki@mikelei.me"
 echo "Thanks to breakwa11 and clowwindy"
 
 if grep -Eqi "CentOS" /etc/issue; then
-    CentOS_install
+    CentOS_Install
 elif grep -Eqi "Ubuntu" /etc/issue; then
-    Ubuntu_install
+    Ubuntu_Install
 else
     Echo_Red "Your distribution is not supported, please use Ubuntu or CentOS to install"
     exit 1
 fi
 
-CentOS_install()
+CentOS_Install()
 {
     Echo_Red "Sorry, CentOS is not supported now"
     exit 1
 }
 
-Ubuntu_install()
+Ubuntu_Install()
 {
     apt-get update && apt-get upgrade -y
     apt-get install python-pip
