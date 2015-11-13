7c0ac21d45f6615d41918ae433ad69dd594629f6
zhangyubaka <zhangyubaka@gmail.com>
2015-11-12 20:47:25 -0600

    Add root detection and add a config file function(Test)

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 echo "One key script by AmedaGintoki@mikelei.me"
 echo "Thanks to breakwa11 and clowwindy"
 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 if grep -Eqi "Arch Linux" /etc/issue; then
     ins="Archlinux"
 elif grep -Eqi "CentOS" /etc/issue; then
@@ -74,8 +79,27 @@ Debian_Install()
 
 Config()
 {
-    echo "Sorry, function does not finish"
-    echo "For more infomation please go to https://github.com/breakwa11/shadowsocks-rss/wiki/Server-Setup(manyuser)"
+    echo "Please enter your sever ip and press [Enter]"
+    read ip
+    echo "Please enter your password and press [Enter]"
+    read pass
+    echo "Please enter server port and press [Enter]"
+    read port
+    echo "Please enter the ciper and press [Enter]"
+    read ciper
+    cat > /etc/shadowsocks.json << "EOF"
+    {
+    "server":"$ip",
+    "server_port":$port,
+    "local_address":"127.0.0.1",
+    "local_port":1080,
+    "password":"$pass",
+    "timeout":300,
+    "method":"$ciper",
+    "workers":1
+    }
+    EOF
+    echo "Config file is create in /etc/shadowsocks.json and local port is 1080"
     exit 1
 }
 
