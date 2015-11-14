6d19f10efb8c053d730cc0815099d122419f4e8b
mikelei8291 <mikelei8291@gmail.com>
2015-11-14 12:56:03 +0800

    Update config.sh

--- include/config.sh
+++ include/config.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-echo "Please input your mysql ip and press [Enter]"
-read ip
-echo "Please input mysql port and press [Enter]"
-read port
-echo "Please input your mysql user and press [Enter]"
-read user
-echo "Please input your mysql password and press [Enter]"
-read pass
-echo "Please input the db name and press [Enter]"
-read db
-rm -f ~/shadowsocks/Config.py
-cat > ~/shadowsocks/Config.py << EOF
+Config()
+{
+    #Config.py
+    echo "Please input your mysql IP address"
+    read ip
+    echo "Please input mysql port"
+    read port
+    echo "Please input your mysql username"
+    read user
+    echo "Please input your mysql password of user ${user}"
+    read pass
+    echo "Please input your database name"
+    read db
+
+    #config.json
+    echo "Please input the encrypt method you want to use"
+    read encrypt
+    echo "Please input the protocol you want to use"
+    read protocol
+    echo "Please input the obfs confusion method you want to use"
+    read obfs
+    echo "Please input the obfs param you want to use"
+    read obfs_param
+
+    rm -f ~/shadowsocks/Config.py
+    rm -f ~/shadowsocks/config.json
+
+    cat > ~/shadowsocks/Config.py << EOF
 #Config
 MYSQL_HOST = '$ip'
 MYSQL_PORT = $port
@@ -25,4 +41,25 @@ MANAGE_BIND_IP = '127.0.0.1'
 #make sure this port is idle
 MANAGE_PORT = 23333
 EOF
-echo "Config file is create in ~/shadowsocks/Config.py"
+    echo "Config file is create in ~/shadowsocks/Config.py"
+
+    cat > ~/shadowsocks/config.json << EOF
+{
+    "server": "0.0.0.0",
+    "server_ipv6": "::",
+    "server_port": 8388,
+    "local_address": "127.0.0.1",
+    "local_port": 1080,
+    "password": "m",
+    "timeout": 120,
+    "method": "$encrypt",
+    "protocol": "$protocol",
+    "obfs": "$obfs",
+    "obfs_param": "$obfs_param",
+    "dns_ipv6": true,
+    "fast_open": true,
+    "workers": 1
+}
+EOF
+    echo "Config file is create in ~/shadowsocks/config.json"
+}
